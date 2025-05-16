05fd845517ea463a616b79ef30a1e639d66a1b80
alexandrecorlet <alexandre.santos@ccc.ufcg.edu.br>
2025-05-16 15:22:21 -0300

    [add] Solve Remove Adjacent Duplicates

---
+++ stacks_and_queues/removeAdjacentDuplicates.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+
+            stack = deque()
+            for c in s:
+                if stack and stack[-1] == c:
+                    stack.pop()
+                    continue
+                stack.append(c)
+            
+            return "".join(stack)
